5f0c743702f4c8b415c18796d01a092c678cb617
Dung <dungnq164@gmail.com>
2015-02-04 10:51:11 +0700

    Fisish Week3_HW2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '8ae743beecbde105ab1a381be3e9d1aeb28cce4345f2a5b9a212194bfdd2236269db619b0f54d3da39d7715d036e4bd7d27fd5dddde32b73212032afef41f987'
